d676afbe2f3b77b035bc6e1799d884da7a10e78f
Tymon Tobolski <i@teamon.eu>
2011-08-01 23:37:23 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Algo::Application.config.secret_token = '105226b53ed3c21de558d8d97503a574a3ef3163412afcd0df2b95b46c6de2023890d5801c8a39bba95eba1f56c79d89e56087f1bd79661b52ac04ca54671f6b'
